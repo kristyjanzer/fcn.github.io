c51d1451ff0a70bf798ee20d27ffabd7a6ef2536
Kristina Yancer <kristy.net.ua@mail.ru>
2025-09-11 15:58:24 +0300

    fix

--- js/common.js
+++ js/common.js
@@ -1,4 +1,28 @@
+// Open menu
 $(function() {
-
+    $('.header-menu-more').click(function() {
+      $(this).toggleClass('active');
+    });
 });
 
+// // Burger Menu
+// const body = document.querySelector("body");
+// const header = document.querySelector(".header");
+// const burgerMenu = document.querySelector(".header .header-button-menu");
+// const burgerBody = document.querySelector(".header .header-button-menu__body");
+
+
+// burgerMenu.addEventListener("click", (e) => {
+//   header.classList.toggle("header-info-open");
+//   burgerBody.classList.toggle("active");
+//   body.classList.toggle("lock");
+//   e.stopPropagation();
+
+
+//   if($('.header-menu-more').hasClass('active')) {
+//     $(".header").addClass("header-info-open");
+//     $(".header .header-button-menu__body").addClass("active");
+//   }
+// });
+
+// $(".header-info").clone().appendTo(".header-button-menu__body");
