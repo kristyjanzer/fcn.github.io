297ca3cf83a6d29bdf830d86f6448b6bd7a98360
Kristina Yancer <kristy.net.ua@mail.ru>
2025-09-11 19:07:39 +0300

    fix

--- js/common.js
+++ js/common.js
@@ -5,24 +5,24 @@ $(function() {
     });
 });
 
-// // Burger Menu
-// const body = document.querySelector("body");
-// const header = document.querySelector(".header");
-// const burgerMenu = document.querySelector(".header .header-button-menu");
-// const burgerBody = document.querySelector(".header .header-button-menu__body");
+// Burger Menu
+const body = document.querySelector("body");
+const header = document.querySelector(".header");
+const burgerMenu = document.querySelector(".header .header-button-menu");
+const burgerBody = document.querySelector(".header .header-button-menu__body");
 
 
-// burgerMenu.addEventListener("click", (e) => {
-//   header.classList.toggle("header-info-open");
-//   burgerBody.classList.toggle("active");
-//   body.classList.toggle("lock");
-//   e.stopPropagation();
+burgerMenu.addEventListener("click", (e) => {
+  header.classList.toggle("header-menu-open");
+  burgerBody.classList.toggle("active");
+  body.classList.toggle("lock");
+  e.stopPropagation();
 
 
-//   if($('.header-menu-more').hasClass('active')) {
-//     $(".header").addClass("header-info-open");
-//     $(".header .header-button-menu__body").addClass("active");
-//   }
-// });
+  if($('.header-menu-more').hasClass('active')) {
+    $(".header").addClass("header-menu-open");
+    $(".header .header-button-menu__body").addClass("active");
+  }
+});
 
-// $(".header-info").clone().appendTo(".header-button-menu__body");
+$(".header-info").clone().appendTo(".header-button-menu__body");
